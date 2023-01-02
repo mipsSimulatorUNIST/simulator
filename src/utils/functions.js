e198ce11e3a271adeabf486607ec8bb48905fac4
limetree06 <limetree06@unist.ac.kr>
2023-01-02 11:33:04 +0900

    [#10] feature : add numToBits function

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -3,23 +3,22 @@ import * as fs from 'fs';
 
 export function numToBits(num) {
   // 10진수 정수를 2진수 bit로 변경해서 return
-  let bits;
-  return bits;
+  return num.toString(2);
 }
 
-export function toHexAndPad(num, pad = 8) { 
+export function toHexAndPad(num, pad = 8) {
   /*
-  * num : Number or String(숫자 형식, 10진법), pad : Number
-  * input : 18 => output: '00000012'
-  * Recommend | num을 Number 타입으로 넣을 것
-  */
-  return Number(num).toString(16).padStart(pad, '0'); 
+   * num : Number or String(숫자 형식, 10진법), pad : Number
+   * input : 18 => output: '00000012'
+   * Recommend | num을 Number 타입으로 넣을 것
+   */
+  return Number(num).toString(16).padStart(pad, '0');
 }
 
 export function symbolTableAddEntry(symbol) {
   SYMBOL_TABLE[symbol.name] = symbol.address;
   if (DEBUG) {
-    log(1, `${symbol.name}: 0x${toHexAndPad(symbol.address)}`)
+    log(1, `${symbol.name}: 0x${toHexAndPad(symbol.address)}`);
   }
 }
 
