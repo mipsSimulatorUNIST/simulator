3223ec13bcf9a8ffc44e418e8ca7ea6908f5855d
limetree06 <limetree06@unist.ac.kr>
2023-01-05 18:31:46 +0900

    [#31] FIX : numToBits function 음수 조건 조정

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -5,7 +5,7 @@ import {exit} from 'process';
 
 export function numToBits(num, pad = 32) {
   // 10진수 정수를 2진수 bit로 변경해서 return
-  if (num > 0) {
+  if (num >= 0) {
     return num.toString(2).padStart(pad, '0'); //양수일때
   } else {
     num = 2 ** pad - 1 + num;
