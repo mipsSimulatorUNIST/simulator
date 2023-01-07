970d2ca68cbcf395dfcd3ce094af1fd3060ea85d
Bams <s.bumsu@gmail.com>
2023-01-08 00:37:00 +0900

    [#33] TEST: Output case 수정
    Output case 여는 부분 수정

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,6 @@
 import {assemble} from '../main.js';
 import {diffString2} from './diff.js';
-import {log, makeInput} from '../src/utils/functions.js';
+import {log, makeInput, makeOutput} from '../src/utils/functions.js';
 import {
   recordDataCase,
   recordDataOutput,
@@ -8,13 +8,12 @@ import {
   recordTextOutput,
   symbolTableCase,
   testInput,
-  testOutput,
   testRecordData,
   testRecordText,
   testSymbolTable,
 } from './testCase.js';
 
-const test = output => {
+const test = (testOutput, output) => {
   diffString2(testOutput, output);
 };
 
@@ -22,13 +21,13 @@ for (let i = 1; i <= 7; i++) {
   const input = makeInput('sample_input', `example${i}.s`);
   log(1, `Input file: sample${i}`);
   const output = assemble(input);
-
-  test(output);
+  const testOutput = makeOutput('sample_output', `example${i}.o`);
+  test(testOutput, output);
 }
 
-// test symbol table
-testSymbolTable(testInput, symbolTableCase);
-// test record text
-testRecordText(recordTextCase, recordTextOutput);
-// test record data
-testRecordData(recordDataCase, recordDataOutput);
+// // test symbol table
+// testSymbolTable(testInput, symbolTableCase);
+// // test record text
+// testRecordText(recordTextCase, recordTextOutput);
+// // test record data
+// testRecordData(recordDataCase, recordDataOutput);
