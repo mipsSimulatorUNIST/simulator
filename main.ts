f7ca012ccb2bab9251138542d6e855412cc61da5
limetree06 <limetree06@unist.ac.kr>
2023-01-31 21:59:44 +0900

    [#73] fix : print

--- main.ts
+++ main.ts
@@ -1,6 +1,12 @@
 import {makeBinaryFile, makeBinaryObject} from './src/simulator/assembler';
-import {initialize} from './src/utils/constants';
-import {mainProcess} from './src/utils/functions';
+import {CYCLES, initialize, initializeMem} from './src/utils/constants';
+import {
+  mainProcess,
+  makeInput,
+  makeOutput,
+  parseSimulatorOutput,
+  simulatorUnitTest,
+} from './src/utils/functions';
 
 export function assemble(assemblyFile: string[]): string {
   const {dataSectionSize, textSectionSize, binaryText, binaryData} =
@@ -19,7 +25,7 @@ export function assemble(assemblyFile: string[]): string {
 export function simulator(assemblyFile: string[], cycle: number): string {
   const {dataSectionSize, textSectionSize, binaryText, binaryData} =
     makeBinaryObject(assemblyFile);
-
+  initializeMem();
   const {INST_INFO} = initialize(
     binaryText.concat(binaryData),
     textSectionSize,
@@ -28,4 +34,14 @@ export function simulator(assemblyFile: string[], cycle: number): string {
 
   const output: string = mainProcess(INST_INFO, cycle);
   return output;
+}
+
+for (let i = 3; i <= 3; i++) {
+  console.log(`testing example ${i}`);
+  simulator(makeInput('sample_input', `example${i}.s`), 10000);
+  const testOutput = parseSimulatorOutput(
+    makeOutput('simulator_sample_output', `example0${i}.o`),
+  );
+  const output = parseSimulatorOutput(CYCLES[CYCLES.length - 1]);
+  simulatorUnitTest(testOutput, output);
 }
