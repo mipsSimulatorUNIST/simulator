e125087bbbe15c6e816f635d48c5b9716727dcaf
haejunejung <jhj9422@unist.ac.kr>
2023-02-06 15:38:51 +0900

    [#77] feat : main.ts 띄어쓰기 및 testcode 제거

--- main.ts
+++ main.ts
@@ -1,39 +1,34 @@
 import {makeBinaryFile, makeBinaryObject} from './src/simulator/assembler';
 import {CYCLES, initialize, initializeMem} from './src/utils/constants';
-import {
-  mainProcess,
-  makeInput,
-  simulatorOutputType,
-} from './src/utils/functions';
+import {mainProcess} from './src/utils/functions';
 
 export function assemble(assemblyFile: string[]): string {
   /*  
    * input : assemblyFile: string[]
    * Enter the path where the assembly file is located.
    
-  If the assemlbyFile path is /Users/junghaejune/simulator/sample_input/example1.s
-  you can enter path /Users/junghaejune/simulator/sample_input/example1.s into assemblyFile.
-   
-  If you don't know the path exactly, you can use makeInput function in functions.ts
-  Let your current directory is simulator (/Users/junghaejune/simulator).
-  Then you only put makeInput("sample_input", "example1.s") into assemblyFile
+   * If the assemlbyFile path is /Users/junghaejune/simulator/sample_input/example1.s
+   * you can enter path /Users/junghaejune/simulator/sample_input/example1.s into assemblyFile.
+   * If you don't know the path exactly, you can use makeInput function in functions.ts
+   * Let your current directory is simulator (/Users/junghaejune/simulator).
+   * Then you only put makeInput("sample_input", "example1.s") into assemblyFile
 
    * output : output: string
    * The assembly file is converted to a string in binary form.
     
-  ex) 
-  input: sample_input/example1.s
-  ...
-  main:
-    and	$17, $17, $0 
-    and	$18, $18, $0
-  ...
-  
-  output:
-  ...
-  00000010001000001000100000100100
-  00000010010000001001000000100100
-  ...
+    ex) 
+    input: sample_input/example1.s
+    ...
+    main:
+      and	$17, $17, $0 
+      and	$18, $18, $0
+    ...
+    
+    output:
+    ...
+    00000010001000001000100000100100
+    00000010010000001001000000100100
+    ...
   */
 
   const {dataSectionSize, textSectionSize, binaryText, binaryData} =
@@ -63,40 +58,40 @@ export function simulator(
    * If returnCycles = false (default), Returns only the final form of the result.
    * If returnCycles = true, Returns an object containing information of all cycles.
    
-  ex) returnCycles = false, you can use this function as below form.
-  const result = simulator(makeInput('sample_input', 'example1.s'), 10000, false)
+    ex) returnCycles = false, you can use this function as below form.
+    const result = simulator(makeInput('sample_input', 'example1.s'), 10000, false)
 
-  ex) returnCycles = true, you can use this function as below form.
-  interface SimulatorResult {
-    output: simulatorOutputType;
-    cycles: simulatorOutputType[];
-  }
+    ex) returnCycles = true, you can use this function as below form.
+    interface SimulatorResult {
+      output: simulatorOutputType;
+      cycles: simulatorOutputType[];
+    }
 
-  const result = simulator(
-    makeInput('sample_input', 'example1.s'),
-    10000,
-    true,
-  ) as SimulatorResult;
+    const result = simulator(
+      makeInput('sample_input', 'example1.s'),
+      10000,
+      true,
+    ) as SimulatorResult;
 
-  * output : The object of Register File.
+    * output : The object of Register File.
 
-  ex)
-  [
-  {
-    PC: '0x00400000',
-    registers: {
-      R0: '0x00000000',
-      R1: '0x00000000',
-      ...
-      R31: '0x00000000'
-    },
-    dataSection: {
-      '0x10000000': '0x00000064',
-      ...
-    },
-    stackSection: {}
-  } 
-  ]
+    ex)
+    [
+    {
+      PC: '0x00400000',
+      registers: {
+        R0: '0x00000000',
+        R1: '0x00000000',
+        ...
+        R31: '0x00000000'
+      },
+      dataSection: {
+        '0x10000000': '0x00000064',
+        ...
+      },
+      stackSection: {}
+    } 
+    ]
   */
   const {dataSectionSize, textSectionSize, binaryText, binaryData} =
     makeBinaryObject(assemblyFile);
@@ -111,16 +106,3 @@ export function simulator(
   const output = mainProcess(INST_INFO, cycle);
   return returnCycles ? {output, cycles: CYCLES} : output;
 }
-
-interface SimulatorResult {
-  output: simulatorOutputType;
-  cycles: simulatorOutputType[];
-}
-
-const result = simulator(
-  makeInput('sample_input', 'example1.s'),
-  2,
-  true,
-) as SimulatorResult;
-
-console.log(result.cycles);
