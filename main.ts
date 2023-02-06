25e6fc1c1bcbaa24342464f26a37637fb457462b
haejunejung <jhj9422@unist.ac.kr>
2023-02-06 15:33:44 +0900

    [#77] feat : main.ts simulator 주석추가

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
 import {makeBinaryFile, makeBinaryObject} from './src/simulator/assembler';
 import {CYCLES, initialize, initializeMem} from './src/utils/constants';
-import {mainProcess} from './src/utils/functions';
+import {
+  mainProcess,
+  makeInput,
+  simulatorOutputType,
+} from './src/utils/functions';
 
 export function assemble(assemblyFile: string[]): string {
   /*  
@@ -50,6 +54,50 @@ export function simulator(
   cycle: number,
   returnCycles = false,
 ): object {
+  /*
+   * input : assemblyFile: string[], cycle: number, returnCycles: boolean
+   * assemblyFile is same as assemblyFile in assemble function above.
+   * cycle is the number of cycles you want to execute.
+   * Executing one cycle means that executing one instruction.
+   * returnCycles determines the type of return.
+   * If returnCycles = false (default), Returns only the final form of the result.
+   * If returnCycles = true, Returns an object containing information of all cycles.
+   
+  ex) returnCycles = false, you can use this function as below form.
+  const result = simulator(makeInput('sample_input', 'example1.s'), 10000, false)
+
+  ex) returnCycles = true, you can use this function as below form.
+  interface SimulatorResult {
+    output: simulatorOutputType;
+    cycles: simulatorOutputType[];
+  }
+
+  const result = simulator(
+    makeInput('sample_input', 'example1.s'),
+    10000,
+    true,
+  ) as SimulatorResult;
+
+  * output : The object of Register File.
+
+  ex)
+  [
+  {
+    PC: '0x00400000',
+    registers: {
+      R0: '0x00000000',
+      R1: '0x00000000',
+      ...
+      R31: '0x00000000'
+    },
+    dataSection: {
+      '0x10000000': '0x00000064',
+      ...
+    },
+    stackSection: {}
+  } 
+  ]
+  */
   const {dataSectionSize, textSectionSize, binaryText, binaryData} =
     makeBinaryObject(assemblyFile);
 
@@ -63,3 +111,16 @@ export function simulator(
   const output = mainProcess(INST_INFO, cycle);
   return returnCycles ? {output, cycles: CYCLES} : output;
 }
+
+interface SimulatorResult {
+  output: simulatorOutputType;
+  cycles: simulatorOutputType[];
+}
+
+const result = simulator(
+  makeInput('sample_input', 'example1.s'),
+  2,
+  true,
+) as SimulatorResult;
+
+console.log(result.cycles);
