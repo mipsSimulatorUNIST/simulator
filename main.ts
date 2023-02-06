21f36d957f8924e122371f2a4f93d319fd5e9423
haejunejung <jhj9422@unist.ac.kr>
2023-02-06 14:44:58 +0900

    [#77] feat : main.ts 주석추가

--- main.ts
+++ main.ts
@@ -3,6 +3,35 @@ import {CYCLES, initialize, initializeMem} from './src/utils/constants';
 import {mainProcess} from './src/utils/functions';
 
 export function assemble(assemblyFile: string[]): string {
+  /*  
+   * input : assemblyFile: string[]
+   * Enter the path where the assembly file is located.
+   
+  If the assemlbyFile path is /Users/junghaejune/simulator/sample_input/example1.s
+  you can enter path /Users/junghaejune/simulator/sample_input/example1.s into assemblyFile.
+   
+  If you don't know the path exactly, you can use makeInput function in functions.ts
+  Let your current directory is simulator (/Users/junghaejune/simulator).
+  Then you only put makeInput("sample_input", "example1.s") into assemblyFile
+
+   * output : output: string
+   * The assembly file is converted to a string in binary form.
+    
+  ex) 
+  input: sample_input/example1.s
+  ...
+  main:
+    and	$17, $17, $0 
+    and	$18, $18, $0
+  ...
+  
+  output:
+  ...
+  00000010001000001000100000100100
+  00000010010000001001000000100100
+  ...
+  */
+
   const {dataSectionSize, textSectionSize, binaryText, binaryData} =
     makeBinaryObject(assemblyFile);
 
