43b8a74f00e31e114710fc0cf20638b677075962
haejunejung <jhj9422@unist.ac.kr>
2023-03-05 19:15:55 +0900

    [#118] feat : github action : merge

---
+++ merge.js
@@ -0,0 +1,81 @@
+import inquirer from 'inquirer';
+import {exec} from 'child_process';
+
+const inquirerAction = async () => {
+  const {action} = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'action',
+      message: 'Are you want to merge?',
+      choices: ['YES', 'NO'],
+    },
+  ]);
+
+  return action;
+};
+
+const inquirerSendingRepository = async () => {
+  const {sendingRepository} = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'sendingRepository',
+      message: '보낼 브랜치 입력',
+      choices: ['main', 'release', 'feature/118/action'],
+    },
+  ]);
+
+  return sendingRepository;
+};
+
+const inquirerReceivngRepository = async () => {
+  const {receivngRepository} = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'receivngRepository',
+      message: '받을 브랜치 입력',
+      choices: ['main', 'release', 'feature/118/mergeCheck'],
+    },
+  ]);
+
+  return receivngRepository;
+};
+
+const inquireConfirmation = async (origin, next) => {
+  const {confirmation} = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'confirmation',
+      message: `내용을 다시 확인하세요. 보낼 브랜치: ${origin} 받는 브랜치: ${next}`,
+      choices: ['확인했어요', '다시 하기 위해서 종료할게요'],
+    },
+  ]);
+
+  return confirmation;
+};
+
+(async () => {
+  const action = await inquirerAction();
+
+  const sendingRepository = await inquirerSendingRepository();
+
+  const receivngRepository = await inquirerReceivngRepository();
+
+  const confirmation = await inquireConfirmation(
+    receivngRepository,
+    sendingRepository,
+  );
+
+  console.log({
+    action,
+    receivngRepository,
+    sendingRepository,
+    confirmation,
+  });
+
+  // `gh workflow` 명령어를 실행하며 사용자에게 입력받은 값을 넘겨 줍니다.
+  if (confirmation === '확인했어요') {
+    exec(
+      `gh workflow run .github/workflows/merge.yml --ref ${sendingRepository} -F SendingRepository=${sendingRepository} -F ReceivingRepository=${receivngRepository}`,
+    );
+  }
+})();
